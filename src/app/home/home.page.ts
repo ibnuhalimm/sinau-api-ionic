902a6d4035e179b2f5a782399f9bdb9f0a95a5a0
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2019-10-23 19:20:35 +0700

    initial project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { DaftarArtikelService } from '../service/daftar-artikel.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
 
-  constructor() {}
+  private artikelSubs: Subscription;
+  artikelList: string[];
+
+  constructor(
+    private artikelSrv: DaftarArtikelService
+  ) {}
+
+
+  ngOnInit() {
+
+    this.artikelSubs = this.artikelSrv.artikelChange.subscribe((artikel) => {
+      this.artikelList = artikel;
+    });
+
+    this.artikelSrv.getDaftarArtikel();
+    // console.log(this.artikelList);
+  }
+
+
+  ngOnDestroy() {
+    this.artikelSubs.unsubscribe();
+  }
 
 }
